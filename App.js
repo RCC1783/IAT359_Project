b7d6b9964b1f5524b255c2dce19d87585f889ae6
RCC1783 <75766592+RCC1783@users.noreply.github.com>
2026-02-10 19:29:27 -0800

    Initial Setup
    Added various empty screens and stuff. Mostly just wanted to test and install dependencies etc.

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='welcome'>
+        <Stack.Screen
+          name="welcome"
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="login"
+        />
+        <Stack.Screen
+          name="home"
+        />
+        <Stack.Screen
+          name="settings"
+        />
+        <Stack.Screen
+          name="allImages"
+        />
+        <Stack.Screen
+          name="projects"
+        />
+        <Stack.Screen
+          name="selectedProject"
+        />
+        <Stack.Screen
+          name="shop"
+        />
+        <Stack.Screen
+          name="projectImages"
+        />
+        <Stack.Screen
+          name="active"
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native/types_generated/index';
+
+import styles from '../styles';
+
+export default function WelcomeScreen() {
+    return(
+        <SafeAreaView>
+            <Text>~ Welcome! ~</Text>
+        </SafeAreaView>
+    );
+}
