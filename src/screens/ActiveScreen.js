9812b6e57a56da80c7b580a8317b6325959007d5
McKenzie Parkhurst <mckenzie_parkhurst@sfu.ca>
2026-02-25 19:52:38 -0800

    Added buttons for Stopwatch and added navigation from projectpage to active

--- src/screens/ActiveScreen.js
+++ src/screens/ActiveScreen.js
@@ -1,13 +1,37 @@
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
 
-import {styles} from '../styles';
-import { useNavigation } from '@react-navigation/native';
+import { styles } from "../styles";
+import { useNavigation } from "@react-navigation/native";
 
 export default function ActiveScreen() {
-    const navigation = useNavigation();
-    return(
-        <SafeAreaView>
-            <Text>~ Active ~</Text>
-        </SafeAreaView>
-    );
+  //For updating the time displaying on the stopwatch
+  const [time, setTime] = useState(0);
+  //To keep track if the timer is running
+  const [running, setRunning] = useState(false);
+
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView>
+      <Text>~ Active ~</Text>
+
+      {/*Text to display the stopwatch for the user*/}
+      <Text>{time}</Text>
+
+      {/*displaying the timer itself*/}
+
+      {/*For Pausing and Resuming the stopwatch; also added reset and Start*/}
+      <Button title="Pause" />
+      <Button title="Resume" />
+      <Button title="Start" />
+      <Button title="Reset" />
+    </SafeAreaView>
+  );
 }
--- src/screens/SelectedProjectScreen.js
+++ src/screens/SelectedProjectScreen.js
@@ -1,16 +1,25 @@
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 
-import {styles} from '../styles';
-import { useNavigation } from '@react-navigation/native';
+import { styles } from "../styles";
+import { useNavigation } from "@react-navigation/native";
 
-export default function SelectedProjectScreen({route}) {
-    const {projectID} = route.params;
-    
-    const navigation = useNavigation();
-    return(
-        <SafeAreaView>
-            <Text>~ selected project ~</Text>
-            <Button title="Shop Screen" onPress={() => navigation.navigate("shop", {project: projectID})}/>
-        </SafeAreaView>
-    );
+export default function SelectedProjectScreen({ route }) {
+  const { projectID } = route.params;
+
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView>
+      <Text>~ selected project ~</Text>
+      <Button
+        title="Shop Screen"
+        onPress={() => navigation.navigate("shop", { project: projectID })}
+      />
+
+      {/* //Button for starting to work on your project */}
+      <Button
+        title="Jump Back In!"
+        onPress={() => navigation.navigate("active")}
+      />
+    </SafeAreaView>
+  );
 }
