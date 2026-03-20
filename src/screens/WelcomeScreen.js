6f4466c21a553849d9640d37320697e1a40d9159
luggi <105999354+LuigiGoesHard69@users.noreply.github.com>
2026-03-19 20:07:30 -0700

    Pull up Image + Delete
    Adjusted welcome screen to create userData if user doesn't have it.
    
    Added delete image and being able to fullscreen it

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -6,6 +6,7 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { auth, db } from '../firebaseConfig';
 import { onAuthStateChanged } from "firebase/auth";
+import { UserData } from '../../globals';
 
 // for this screen i need an await to see if the user is logged in, if they are, send them to home, if not send them to login
 // im assuming this is like a splash screen? :O
@@ -23,9 +24,6 @@ export default function WelcomeScreen() {
                 allUsers.push(JSON.stringify(doc.id));
             })
 
-            console.log(savedUID);
-            console.log(allUsers);
-
             if (savedUID) {
                 if (allUsers.includes(savedUID)) {
                     console.log("User is logged in, navigating to home screen: ", savedUID);
@@ -40,6 +38,22 @@ export default function WelcomeScreen() {
             }
         } catch (error) {
             console.error("Error fetching users:", error);
+        }
+
+        try {
+            const savedUID = await AsyncStorage.getItem('uid');
+            let userData = await AsyncStorage.getItem(savedUID);
+
+            userData = JSON.parse(userData);
+
+            if (!userData) {
+                userData = new UserData;
+                await AsyncStorage.setItem(savedUID, JSON.stringify(userData));
+            } else {
+                console.log("user data found:", userData);
+            }
+        } catch (error) {
+            console.log(error.message);
         }
     }
 
